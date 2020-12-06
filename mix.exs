4bc57140765c22fcd39f819521c2e3253e285c29
galina <galina.lezina@gmail.com>
2020-12-06 21:02:24 +0500

    add project info

--- mix.exs
+++ mix.exs
@@ -5,8 +5,21 @@ defmodule Qew.MixProject do
     [
       app: :qew,
       version: "0.1.0",
-      elixir: "~> 1.11",
-      start_permanent: Mix.env() == :prod
+      elixir: "~> 1.10",
+      start_permanent: Mix.env() == :prod,
+      description: "Elixir wrapper around erlang :queue module. Module keeps track of the queue length.",
+      package: package()
+    ]
+  end
+
+  defp package do
+    [
+      name: :qew,
+      files: ["lib", "mix.exs", "README*"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/galina/qew"
+      }
     ]
   end
 end
