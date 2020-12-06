095399ab7b6d318fdb592a8e7f00d48d01342b15
galina <galina.lezina@gmail.com>
2020-12-06 16:56:09 +0500

    implement queue wrapper

--- lib/qew.ex
+++ lib/qew.ex
@@ -1,18 +1,139 @@
 defmodule Qew do
   @moduledoc """
-  Documentation for `Qew`.
+  Wrapper around erlang `:queue` module.
+
+  Module keeps track of the queue length.
   """
+
+  @doc false
+  def to_list({_queue, 0}), do: []
+  def to_list({queue, _size}), do: :queue.to_list(queue)
+
+  @doc false
+  def from_list([]), do: empty()
+
+  def from_list(list) do
+    Qew.enqueue_list(empty(), list)
+  end
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Qew.hello()
-      :world
-
+  Drops element from queue.
   """
-  def hello do
-    :world
+  def drop({queue, 0}), do: {queue, 0}
+  def drop({queue, size}), do: {:queue.drop(queue), size - 1}
+
+  @doc """
+  Drops n elements from queue.
+  """
+  def drop_n({queue, 0}), do: queue
+
+  def drop_n(queue, n) when is_integer(n) do
+    Enum.reduce_while(1..n, queue, fn _, acc ->
+      acc = drop(acc)
+
+      if empty?(acc) do
+        {:halt, acc}
+      else
+        {:cont, acc}
+      end
+    end)
+  end
+
+  @doc """
+  Enqueue list elements.
+  """
+  def enqueue_list(queue, values) do
+    Enum.reduce(values, queue, fn value, acc -> enqueue(acc, value) end)
+  end
+
+  @doc """
+  Joins two queues.
+  """
+  def join(queue1, {_, 0}), do: queue1
+  def join({_, 0}, queue2), do: queue2
+
+  def join(queue1, {_, n} = queue2) do
+    {queue1, _} =
+      Enum.reduce(1..n, {queue1, queue2}, fn _, {queue1, queue2} ->
+        {value, queue2} = dequeue(queue2)
+
+        {enqueue(queue1, value), queue2}
+      end)
+
+    queue1
+  end
+
+  @doc """
+  Splits queue in two. First queue contains n elements.
+  """
+  def split(queue, 0), do: {queue, empty()}
+
+  def split(queue, n) when is_integer(n) do
+    if size(queue) <= n do
+      {queue, empty()}
+    else
+      split_n(queue, n)
+    end
+  end
+
+  @doc """
+  Returns queue size.
+  """
+  def size({_, size}), do: size
+
+  @doc """
+  Returns empty queue.
+  """
+  def empty, do: {:queue.new(), 0}
+
+  def empty?({_queue, size}), do: size == 0
+
+  @doc """
+  Returns head element.
+
+  Returns `nil` if queue is empty.
+  """
+  def peek({_queue, 0}), do: nil
+
+  def peek({queue, _size}) do
+    {:value, value} = :queue.peek(queue)
+
+    value
+  end
+
+  @doc """
+  Dequeues head element.
+
+  Returns {nil, empty_queue} if queue is empty.
+  Returns {value, updated_queue} if queue is not empty.
+  """
+  def dequeue({_, 0} = q), do: {nil, q}
+
+  def dequeue({queue, size}) do
+    {{:value, value}, queue} = :queue.out(queue)
+
+    {value, {queue, size - 1}}
+  end
+
+  @doc """
+  Add element into queue head.
+  """
+  def enqueue_head({queue, size}, value) do
+    {:queue.in_r(value, queue), size + 1}
+  end
+
+  @doc """
+  Enqueue element.
+  """
+  def enqueue({queue, size}, value) do
+    {:queue.in(value, queue), size + 1}
+  end
+
+  defp split_n(queue, n) do
+    Enum.reduce(1..n, {empty(), queue}, fn _, {queue1, queue2} ->
+      {value, queue2} = dequeue(queue2)
+
+      {enqueue(queue1, value), queue2}
+    end)
   end
 end
--- test/qew_test.exs
+++ test/qew_test.exs
@@ -1,8 +1,209 @@
 defmodule QewTest do
   use ExUnit.Case
-  doctest Qew
 
-  test "greets the world" do
-    assert Qew.hello() == :world
+  describe "drop/1" do
+    test "does nothing with empty queue" do
+      assert Qew.drop(Qew.empty()) == Qew.empty()
+    end
+
+    test "drops head if queue is not empty" do
+      queue = Qew.from_list([1, 2, 3])
+      assert Qew.to_list(queue) == [1, 2, 3]
+
+      queue = Qew.drop(queue)
+      assert Qew.to_list(queue) == [2, 3]
+
+      queue = Qew.drop(queue)
+      assert Qew.to_list(queue) == [3]
+
+      queue = Qew.drop(queue)
+      assert Qew.to_list(queue) == []
+    end
+  end
+
+  describe "drop_n/2" do
+    test "does nothing with empty queue" do
+      assert Qew.drop_n(Qew.empty(), 20) == Qew.empty()
+    end
+
+    test "drops n items" do
+      initial_queue = Qew.from_list([1, 2, 3])
+
+      queue = Qew.drop_n(initial_queue, 20)
+      assert Qew.to_list(queue) == []
+
+      queue = Qew.drop_n(initial_queue, 3)
+      assert Qew.to_list(queue) == []
+
+      queue = Qew.drop_n(initial_queue, 2)
+      assert Qew.to_list(queue) == [3]
+
+      queue = Qew.drop_n(initial_queue, 1)
+      assert Qew.to_list(queue) == [2, 3]
+    end
+  end
+
+  describe "size/1" do
+    test "returns empty queue size" do
+      assert Qew.size(Qew.empty()) == 0
+    end
+
+    test "returns queue size" do
+      assert Qew.size(Qew.from_list([1, 2, 3, 4, 5])) == 5
+      assert Qew.size(Qew.from_list([1])) == 1
+      assert Qew.size(Qew.from_list([])) == 0
+    end
+  end
+
+  describe "peek/1" do
+    test "peeks empty queue" do
+      assert Qew.peek(Qew.empty()) == nil
+    end
+
+    test "peeks non empty queue" do
+      assert Qew.peek(Qew.from_list([12, 2, 3, 4, 5])) == 12
+    end
+  end
+
+  describe "dequeue/1" do
+    test "dequeues nil if queue is empty" do
+      assert Qew.dequeue(Qew.empty()) == {nil, Qew.empty()}
+    end
+
+    test "returns head value" do
+      initial_queue = Qew.from_list([6, 1, 2, 3, 4, 5])
+
+      assert {6, queue} = Qew.dequeue(initial_queue)
+      assert Qew.to_list(queue) == [1, 2, 3, 4, 5]
+
+      assert {1, queue} = Qew.dequeue(queue)
+      assert Qew.to_list(queue) == [2, 3, 4, 5]
+
+      assert {5, queue} = Qew.dequeue(Qew.from_list([5]))
+      assert Qew.to_list(queue) == []
+    end
+  end
+
+  describe "enqueue/1" do
+    test "enqueues element into empty queue" do
+      queue = Qew.enqueue(Qew.empty(), 8)
+
+      assert Qew.to_list(queue) == [8]
+    end
+
+    test "enqueues element if queue is not empty" do
+      initial_queue = Qew.from_list([6, 1, 2, 3, 4, 5])
+
+      queue = Qew.enqueue(initial_queue, 18)
+      assert Qew.to_list(queue) == [6, 1, 2, 3, 4, 5, 18]
+    end
+  end
+
+  describe "enqueue_head/2" do
+    test "prepends element into empty queue" do
+      queue = Qew.enqueue(Qew.empty(), 8)
+
+      assert Qew.to_list(queue) == [8]
+    end
+
+    test "prepends element if queue is not empty" do
+      initial_queue = Qew.from_list([6, 1, 2, 3, 4, 5])
+
+      queue = Qew.enqueue_head(initial_queue, 18)
+      assert Qew.to_list(queue) == [18, 6, 1, 2, 3, 4, 5]
+    end
+  end
+
+  describe "empty/1" do
+    test "returns new queue" do
+      assert Qew.empty() == {:queue.new(), 0}
+    end
+  end
+
+  describe "split/2" do
+    test "does nothing with empty queue" do
+      assert Qew.split(Qew.empty(), 0) == {Qew.empty(), Qew.empty()}
+      assert Qew.split(Qew.empty(), 1) == {Qew.empty(), Qew.empty()}
+      assert Qew.split(Qew.empty(), 10) == {Qew.empty(), Qew.empty()}
+      assert Qew.split(Qew.from_list([]), 3) == {Qew.empty(), Qew.empty()}
+    end
+
+    test "splits non empty queue" do
+      {queue1, queue2} = Qew.split(Qew.from_list([1]), 0)
+      assert Qew.to_list(queue1) == [1]
+      assert queue2 == Qew.empty()
+
+      {queue1, queue2} = Qew.split(Qew.from_list([1, 2, 3]), 0)
+      assert Qew.to_list(queue1) == [1, 2, 3]
+      assert queue2 == Qew.empty()
+
+      {queue1, queue2} = Qew.split(Qew.from_list([1]), 4)
+      assert Qew.to_list(queue1) == [1]
+      assert queue2 == Qew.empty()
+
+      {queue1, queue2} = Qew.split(Qew.from_list([1]), 1)
+      assert Qew.to_list(queue1) == [1]
+      assert queue2 == Qew.empty()
+
+      {queue1, queue2} = Qew.split(Qew.from_list([11, 13]), 1)
+      assert Qew.to_list(queue1) == [11]
+      assert Qew.to_list(queue2) == [13]
+
+      {queue1, queue2} = Qew.split(Qew.from_list([13, 11]), 1)
+      assert Qew.to_list(queue1) == [13]
+      assert Qew.to_list(queue2) == [11]
+
+      {queue1, queue2} = Qew.split(Qew.from_list([13, 11, 14]), 1)
+      assert Qew.to_list(queue1) == [13]
+      assert Qew.to_list(queue2) == [11, 14]
+
+      {queue1, queue2} = Qew.split(Qew.from_list([13, 11, 14]), 2)
+      assert Qew.to_list(queue1) == [13, 11]
+      assert Qew.to_list(queue2) == [14]
+
+      {queue1, queue2} = Qew.split(Qew.from_list([13, 17, 15]), 3)
+      assert Qew.to_list(queue1) == [13, 17, 15]
+      assert queue2 == Qew.empty()
+    end
+  end
+
+  describe "join/2" do
+    test "joins empty queues" do
+      assert Qew.join(Qew.empty(), Qew.empty()) == Qew.empty()
+    end
+
+    test "joins queues" do
+      joined = Qew.join(Qew.from_list([9, 81, 4]), Qew.empty())
+      assert Qew.to_list(joined) == [9, 81, 4]
+
+      joined = Qew.join(Qew.empty(), Qew.from_list([15, 0]))
+      assert Qew.to_list(joined) == [15, 0]
+
+      joined = Qew.join(Qew.from_list([1]), Qew.from_list([2]))
+      assert Qew.to_list(joined) == [1, 2]
+
+      joined = Qew.join(Qew.from_list([2]), Qew.from_list([1]))
+      assert Qew.to_list(joined) == [2, 1]
+    end
+  end
+
+  describe "enqueue_list/2" do
+    test "enqueues list into empty queue" do
+      queue = Qew.enqueue_list(Qew.empty(), [4, 7, 10])
+      assert Qew.to_list(queue) == [4, 7, 10]
+
+      queue = Qew.enqueue_list(Qew.empty(), [])
+      assert Qew.to_list(queue) == []
+    end
+
+    test "enqueues list into non empty queue" do
+      initial_queue = Qew.from_list([9, 0, 3])
+
+      queue = Qew.enqueue_list(initial_queue, [4, 7, 10])
+      assert Qew.to_list(queue) == [9, 0, 3, 4, 7, 10]
+
+      queue = Qew.enqueue_list(initial_queue, [])
+      assert Qew.to_list(queue) == [9, 0, 3]
+    end
   end
 end
